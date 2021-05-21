0c0f90bc6d33f1e771a404cf2b93d71788c9e6d9
Moulham Hallak <mou990@gmail.com>
2021-05-21 12:38:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+class Cat {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak() {
+    return `${this.name} says meow`;
+  }
+}
+
+let transaction = new Transaction(100.24, '03/04/2018', 'Grocery Shopping');
+ 
+class Dog {
+  constructor(name, sex) {
+  this.name = name;
+  this.sex = sex;
+  }
+  speak() {
+    return `${this.name} says woof`;
+  }
+}
+ 
+class Bird {
+  constructor(name, sex) {
+  this.name = name;
+  this.sex = sex;
+  }
+  speak() {
+    return `${this.name} says squak`;
+  }
+}
